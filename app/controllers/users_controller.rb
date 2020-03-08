5bda009a492bd047be10a1ba33ebb8efca1e7a8d
takepon <tqr1104@yahoo.co.jp>
2020-03-08 21:01:40 +0900

    ログイン中の挙動の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user, {only: [:index, :show, :edit, :update]}
-  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login, :top]}
+
+  def top
+  end
 
   def index
     @users = User.all
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   get 'home/top'
   get 'home/login'
   get 'home/signup'
-  root 'home#top'
+  root 'users#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
